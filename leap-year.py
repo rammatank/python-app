165a7698d7b65f8f32c830916c45afe68a11d42a
rammat <rammatank06@gmail.com>
2022-07-30 16:32:07 +0300

    added new line

--- leap-year.py
+++ leap-year.py
@@ -1,6 +1,5 @@
 
-
-
+merhaba
 
 # Exercise | ?comprehension | ?range() | ?len() | ?nested for loop() | ?any() | ?sum() | ?append()**
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
